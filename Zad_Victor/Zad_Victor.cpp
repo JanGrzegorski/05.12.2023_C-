6c22b27c2d56cf0dd5fb1cfa9f4ae9f8ccdec5f1
pawelce <szkolacecot@gmail.com>
2023-12-05 10:29:56 +0100

    kiecka2

---
+++ Zad_Victor/Zad_Victor.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    srand(time(NULL));
+    vector <int> tablica;
+
+    for (size_t i = 0; i < 5; i++)
+    {
+        int los = rand() % 100 + 1;
+        tablica.push_back(los);
+    }
+    sort(tablica.begin(), tablica.end());
+    for (size_t i = 0; i < tablica.size(); i++) {
+        cout << tablica[i] << endl;
+    }
+    {
+
+    }
+    return EXIT_SUCCESS;
+}
+
