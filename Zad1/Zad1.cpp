d350c20ade796b93b6dd18b69ebb71a07c91da86
pawelce <szkolacecot@gmail.com>
2023-12-05 09:55:38 +0100

    kiecka

---
+++ Zad1/Zad1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int rozmiar;
+
+	cout << "Podaj rozmiar tablicy imion = "; cin >> rozmiar;
+
+	string* imiona = new string[rozmiar];
+
+	for (size_t i = 0; i < rozmiar; i++) {
+		cout << "Podaj imie " << i+1 << "= ";cin >> imiona[i];
+	}
+
+	for (size_t i = 0; i < rozmiar; i++) {
+		
+		if (i == rozmiar - 1)	cout << imiona[i];
+		else cout << imiona[i] << ", ";
+	}
+	delete[]imiona;
+
+	return EXIT_SUCCESS;
+}
